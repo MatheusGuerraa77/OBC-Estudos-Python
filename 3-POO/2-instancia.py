ffc8f181fca0e01d0bd2b32d9cba85d14efa07a5
MatheusGuerraa77 <matheusaraujoguerra2603@outlook.com>
2024-04-19 11:52:01 -0300

    Update 2-instancia.py

--- 3-POO/2-instancia.py
+++ 3-POO/2-instancia.py
@@ -14,7 +14,17 @@ movie.note = 5.0
 movie.durationMinutes = 130
 
 #Segundo filme
+movie2 = Movie()
+movie2.name = "Kung Fu Panda 4"
+movie2.yearLaunch = 2024
+movie2.viewincluded = False
+movie2.note = 7.0
+movie2.durationMinutes = 90
+
+
 
 
 print("##Dados do Filme##")
 print(f"Nome do filme : {movie.name}  \n Ano de lançamento: {movie.yearLaunch}")
+print("##Dados do Filme 2##")
+print(f"Nome do filme : {movie2.name}  \n Ano de lançamento: {movie2.yearLaunch}")
